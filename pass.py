cb24ab141e47386e5184fd8f2fc6a022ab3aa163
Timon <timon@example.com>
2018-04-09 16:15:51 +0200

    change new

--- pass.py
+++ pass.py
@@ -1,7 +1,6 @@
-#hallo
-import numpy as np
 import pickle
-
+import random
+import string
 def get_credentials():
     username = input('Please type your user name: ')
     password = input('Please type your password: ')
@@ -10,7 +9,7 @@ def get_credentials():
 def authenticate(username, password, pwdb):
     status = False
     if username in pwdb:
-        if pwdb[username] == password:
+        if pwdb[username][0] == get_hash(password + pwdb[username][1]):
             status = True
     else:
         ans = input('User not known. Add it to db? [y/n]')
@@ -21,7 +20,8 @@ def authenticate(username, password, pwdb):
 
 def add_user(username, password, pwdb):
     if username not in pwdb:
-        pwdb[username] = password
+        salt = make_hash_salt(3)
+        pwdb[username] = (str(get_hash(password) + str(salt)), str(salt))   #salt should be same for each user
         write_pwdb(pwdb)
     else:
         print('User already known!')
@@ -43,9 +43,27 @@ def write_pwdb(pwdb):
 def get_path():
     return 'pwdb.pkl'
 
+def get_hash(password): #entered password includes salt
+    numerator = 1
+    res = 0
+    for ch in str(password):
+        res += ord(ch)*numerator
+        #print('res')
+        #print(res)
+        #print('ord char')
+        #print(ord(ch))
+        numerator += 1
+    return res
+
+def make_hash_salt(N):
+    salt = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+    return salt
+
+
 pwdb = read_pwdb()
 username, password = get_credentials()
-if authenticate(username, password, pwdb):
+hash = get_hash(password+make_hash_salt(3))
+if authenticate(username, hash, pwdb):
     print(pwdb)
 else:
     print('No match!')
