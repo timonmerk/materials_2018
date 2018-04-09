2dc3a8b26b21f5bd1832ac6b7be8b374129665cc
Tiziano Zito <opossumnano@gmail.com>
2018-04-09 11:08:00 +0200

    implemented authenticate. PW database still missing

--- pass.py
+++ pass.py
@@ -1,5 +1,18 @@
 def get_credentials():
     username = input('Please type your user name: ')
     password = input('Please type your password: ')
+    return username, password
 
-get_credentials()
+def authenticate(username, password, pwdb):
+    if username in pwdb:
+        if pwdb[username] == password:
+            return True
+    return False
+
+pwdb = {'tiziano': 'right_password'}
+username, password = get_credentials()
+if authenticate(username, password, pwdb):
+    print('Match!')
+else:
+    print('No match!')
+
